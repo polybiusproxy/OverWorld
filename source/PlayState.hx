70d918e36eb0e9fa294a6f5e1db1a576e8b7b90c
expLOIT <herotutos89@gmail.com>
2021-05-17 15:29:28 +0200

    debug shit

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,12 +1,23 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxState;
+import flixel.text.FlxText;
 
 class PlayState extends FlxState
 {
+	var dimensionsText:FlxText;
+	var overworldInfo:FlxText;
+
 	override public function create()
 	{
 		super.create();
+
+		dimensionsText = new FlxText(0, 0, "Dimensions: " + FlxG.width + "x" + FlxG.height, 15);
+		add(dimensionsText);
+
+		overworldInfo = new FlxText(0, dimensionsText.y + 20, "OverWorld - " + FlxG.VERSION, 15);
+		add(overworldInfo);
 	}
 
 	override public function update(elapsed:Float)
