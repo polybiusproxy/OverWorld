83292eb651ed791b8b1ecd9fa005ba1b9af0a4be
expLOIT <herotutos89@gmail.com>
2021-05-17 16:43:15 +0200

    player & ground test

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,13 +1,18 @@
 package;
 
 import flixel.FlxG;
+import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.text.FlxText;
+import flixel.util.FlxColor;
 
 class PlayState extends FlxState
 {
 	var dimensionsText:FlxText;
 	var overworldInfo:FlxText;
+
+	var daPlayer = new FlxSprite();
+	var daGround = new FlxSprite();
 
 	override public function create()
 	{
@@ -18,6 +23,14 @@ class PlayState extends FlxState
 
 		overworldInfo = new FlxText(0, dimensionsText.y + 20, "OverWorld - " + FlxG.VERSION, 15);
 		add(overworldInfo);
+
+		daPlayer.makeGraphic(100, 100, FlxColor.WHITE);
+		daPlayer.screenCenter();
+		add(daPlayer);
+
+		daGround.makeGraphic(FlxG.width, 360, FlxColor.RED);
+		daGround.screenCenter();
+		add(daGround);
 	}
 
 	override public function update(elapsed:Float)
